d3ce9c88530e288d04413e962e99db0d53c2777a
Nekmo <contacto@nekmo.com>
2020-05-08 00:21:25 +0200

    Issue #8: Test template directories

--- django_code_generator/management/commands/generator.py
+++ django_code_generator/management/commands/generator.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 from django_code_generator.templates import Template, get_template_directory
 
 TEMPLATES_ENVIRONMENT_VARIABLE = 'DJANGO_CODE_GENERATOR_TEMPLATES'
+TEMPLATES_DJANGO_SETTINGS_VARIABLE = 'DJANGO_CODE_GENERATOR_TEMPLATES'
 TEMPLATE_DIRECTORY = 'dcg_templates'
 DOTTED_TEMPLATE_DIRECTORY = '.dcg_templates'
 directory = os.path.abspath(os.path.join(os.path.abspath(__file__), '../../../'))
@@ -20,22 +21,24 @@ else:
     main_tpl_directory = os.path.join(os.path.abspath(__main__.__file__), DOTTED_TEMPLATE_DIRECTORY)
 
 
-# Get directories from DJANGO_CODE_GENERATOR_TEMPLATES environment variable
-template_directories = os.environ.get(TEMPLATES_ENVIRONMENT_VARIABLE, '').split(':')
-# Get templates from the folder DOTTED_TEMPLATE_DIRECTORY in current directory
-template_directories += [
-    os.path.join(os.getcwd(), DOTTED_TEMPLATE_DIRECTORY),
-]
-# Get templates from DJANGO_CODE_GENERATOR_TEMPLATES settings
-template_directories.extend(getattr(settings, 'DJANGO_CODE_GENERATOR_TEMPLATES', []))
-template_directories += [
-    # Get templates from folder DOTTED_TEMPLATE_DIRECTORY in manage.py directory
-    main_tpl_directory,
-    # Get templates from .config/TEMPLATE_DIRECTORY in home directory
-    os.path.join(expanduser("~/.config/"), TEMPLATE_DIRECTORY),
-    # Get default templates from django_code_generator project
-    os.path.join(directory, 'templates'),
-]
+def get_template_directories():
+    # Get directories from DJANGO_CODE_GENERATOR_TEMPLATES environment variable
+    template_directories = os.environ.get(TEMPLATES_ENVIRONMENT_VARIABLE, '').split(':')
+    # Get templates from the folder DOTTED_TEMPLATE_DIRECTORY in current directory
+    template_directories += [
+        os.path.join(os.getcwd(), DOTTED_TEMPLATE_DIRECTORY),
+    ]
+    # Get templates from DJANGO_CODE_GENERATOR_TEMPLATES settings
+    template_directories.extend(getattr(settings, TEMPLATES_DJANGO_SETTINGS_VARIABLE, []))
+    template_directories += [
+        # Get templates from folder DOTTED_TEMPLATE_DIRECTORY in manage.py directory
+        main_tpl_directory,
+        # Get templates from .config/TEMPLATE_DIRECTORY in home directory
+        os.path.join(expanduser("~/.config/"), TEMPLATE_DIRECTORY),
+        # Get default templates from django_code_generator project
+        os.path.join(directory, 'templates'),
+    ]
+    return template_directories
 
 
 class Command(BaseCommand):
@@ -46,4 +49,4 @@ class Command(BaseCommand):
         parser.add_argument('app', type=str)
 
     def handle(self, *args, **options):
-        Template(get_template_directory(template_directories, options['template']), options['app']).render()
+        Template(get_template_directory(get_template_directories(), options['template']), options['app']).render()
--- django_code_generator/tests/test_django_code_generator.py
+++ django_code_generator/tests/test_django_code_generator.py
@@ -2,21 +2,42 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `django_code_generator` package."""
-
-
+import os
 import unittest
+from os.path import expanduser
+from unittest.mock import patch
 
-from django_code_generator import django_code_generator
+from django.test import override_settings
+
+from django_code_generator.management.commands.generator import TEMPLATES_ENVIRONMENT_VARIABLE, \
+    TEMPLATES_DJANGO_SETTINGS_VARIABLE, get_template_directories, DOTTED_TEMPLATE_DIRECTORY, TEMPLATE_DIRECTORY, \
+    directory
 
 
-class TestDjango_code_generator(unittest.TestCase):
-    """Tests for `django_code_generator` package."""
+class TestGetTemplateDirectories(unittest.TestCase):
 
     def setUp(self):
-        """Set up test fixtures, if any."""
+        module = 'django_code_generator.management.commands.generator'
+        self.env_mock = patch.dict('os.environ', {TEMPLATES_ENVIRONMENT_VARIABLE: '/env/'})
+        self.env_mock.start()
+        self.main_mock = patch('{}.main_tpl_directory'.format(module),
+                             '/main/')
+        self.main_mock.start()
 
     def tearDown(self):
-        """Tear down test fixtures, if any."""
+        self.main_mock.stop()
+        self.env_mock.stop()
 
-    def test_000_something(self):
+    # @patch.dict('os.environ', {TEMPLATES_ENVIRONMENT_VARIABLE: '/env/'})
+    @override_settings(**{TEMPLATES_DJANGO_SETTINGS_VARIABLE: ['/settings/']})
+    def test_directories(self):
         """Test something."""
+        directories = get_template_directories()
+        self.assertListEqual(directories, [
+            '/env/',
+            os.path.join(os.getcwd(), DOTTED_TEMPLATE_DIRECTORY),
+            '/settings/',
+            '/main/',
+            os.path.join(expanduser("~/.config/"), TEMPLATE_DIRECTORY),
+            os.path.join(directory, 'templates'),
+        ])
